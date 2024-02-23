816897fe262ebea5eab4867895eca55ad357be9f
JVPH <gyrospizzeria@protonmail.com>
2024-02-22 20:03:02 -0500

    reset users.py

--- app/resources/users.py
+++ app/resources/users.py
@@ -1,7 +1,2 @@
-from flask import Blueprint
 
-main_routes = Blueprint('main_routes', __name__)
 
-@main_routes.route('/')
-def index():
-    return '¡La aplicación Flask está funcionando correctamente!'
