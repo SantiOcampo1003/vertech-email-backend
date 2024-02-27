d021082fe132e23eb13c71c59672b20b02b07fa9
JVPH <gyrospizzeria@protonmail.com>
2024-02-27 16:07:34 -0500

    add find_by_u_email method, update json method

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from app.database.db import db
 
+
 class UserModel(db.Model):
     __tablename__ = "users"
 
@@ -8,16 +9,19 @@ class UserModel(db.Model):
     u_email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
 
-    sent_emails = db.relationship("EmailModel", back_populates="sender", foreign_keys="[EmailModel.sender_id]", lazy="dynamic")
-    received_emails = db.relationship("EmailModel", back_populates="recipient", foreign_keys="[EmailModel.recipient_id]", lazy="dynamic")
+    sent_emails = db.relationship("EmailModel", back_populates="sender", foreign_keys="[EmailModel.sender_id]",
+                                  lazy="dynamic")
+    received_emails = db.relationship("EmailModel", back_populates="recipient",
+                                      foreign_keys="[EmailModel.recipient_id]", lazy="dynamic")
+
+    @classmethod
+    def find_by_u_email(cls, email):
+        return cls.query.filter_by(u_email=email).first()
 
     def json(self):
         return {
-            "id": self.id,
             "name": self.name,
-            "email": self.u_email,
-            "received_emails": self.received_emails,
-            "sent_emails": self.sent_emails
+            "u_email": self.u_email,
         }
 
     def save_to_db(self):
