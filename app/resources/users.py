e81a4b8a684a86fa70a8473da315f76510e7e0d9
Julian <julian.ramirez04@uptc.edu.co>
2024-03-17 02:24:52 -0500

    The code is documented

--- app/resources/users.py
+++ app/resources/users.py
@@ -1,10 +1,11 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt, create_refresh_token, get_jwt_identity
+from flask_jwt_extended import create_access_token, jwt_required
 from datetime import timedelta
 from ..models.user import UserModel
-from ..schemas import UserSchema, UserLoginSchema
+from ..schemas.UserSchema import UserSchema, UserLoginSchema
+from ..schemas.PlainSchema import PlainUserSchema
 
 from sqlalchemy.exc import IntegrityError
 
@@ -13,8 +14,23 @@ blp = Blueprint("users", __name__, description="Operations on users.")
 
 @blp.route("/api/register")
 class UserRegister(MethodView):
+    """
+    Method view for registering a new user.
+
+    Attributes:
+        post: Handles POST requests for user registration.
+    """
     @blp.arguments(UserSchema)
     def post(self, user_data):
+        """
+        Handle POST request to register a new user.
+
+        Args:
+            user_data (dict): The data for user registration.
+
+        Returns:
+            tuple: A tuple containing a success message and HTTP status code 201.
+        """
         user = UserModel(
             name=user_data["name"],
             u_email=user_data["u_email"],
@@ -33,8 +49,24 @@ class UserRegister(MethodView):
 
 @blp.route("/api/login")
 class UserLogin(MethodView):
+    """
+    Method view for user login.
+
+    Attributes:
+        post: Handles POST requests for user login.
+    """
     @blp.arguments(UserLoginSchema)
     def post(self, user_data):
+        """
+        Handle POST request for user login.
+
+        Args:
+            user_data (dict): The data for user login.
+
+        Returns:
+            tuple: A tuple containing an access token and user details if login is successful, 
+                   otherwise raises HTTP status code 401.
+        """
         user = UserModel.find_by_u_email(user_data["u_email"])
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
@@ -42,3 +74,24 @@ class UserLogin(MethodView):
             return {"access_token": access_token, **user.json()}, 200
 
         abort(401, message="Invalid credentials.")
+
+
+@blp.route("/api/users")
+class Users(MethodView):
+    """
+    Method view for retrieving users.
+
+    Attributes:
+        get: Handles GET requests for retrieving users.
+    """
+    @jwt_required()
+    @blp.response(200, PlainUserSchema(many=True))
+    def get(self):
+        """
+        Handle GET request to retrieve all users.
+
+        Returns:
+            list: A list of user details.
+        """
+        users = UserModel.query.all()
+        return users
--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,14 @@ from marshmallow import Schema, fields
 
 
 class PlainUserSchema(Schema):
+    """
+    Schema for representing plain user data.
+
+    Attributes:
+        name (str): The name of the user.
+        u_email (str): The email of the user.
+        password (str): The password of the user.
+    """
     #id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     u_email = fields.Email(required=True)
@@ -10,6 +18,15 @@ class PlainUserSchema(Schema):
 
 
 class PlainEmailSchema(Schema):
+    """
+    Schema for representing plain email data.
+
+    Attributes:
+        id (int): The ID of the email.
+        subject (str): The subject of the email.
+        body (str): The body of the email.
+        timestamp (datetime): The timestamp of the email.
+    """
     id = fields.Int(dump_only=True)
     subject = fields.Str(required=True)
     body = fields.Str(required=True)
@@ -17,22 +34,52 @@ class PlainEmailSchema(Schema):
 
 
 class EmailFormSchema(Schema):
+    """
+    Schema for validating email form data.
+
+    Attributes:
+        subject (str): The subject of the email.
+        timestamp (datetime): The timestamp of the email.
+        body (str): The body of the email.
+        recipient_email (str): The email address of the recipient.
+    """
     subject = fields.Str(required=True)
     timestamp = fields.DateTime()
     body = fields.Str(required=True)
     recipient_email = fields.Email(required=True)
 
 class UserLoginSchema(Schema):
+    """
+    Schema for validating user login data.
+
+    Attributes:
+        u_email (str): The email of the user.
+        password (str): The password of the user.
+    """
     u_email = fields.Email(required=True)
     password = fields.Str(required=True, load_only=True)
 
 
 class EmailSchema(PlainEmailSchema):
+    """
+    Schema for representing email data.
+
+    Attributes:
+        sender (PlainUserSchema): The sender of the email.
+        recipient (PlainUserSchema): The recipient of the email.
+    """
     sender = fields.Nested(PlainUserSchema(), dump_only=True)
     recipient = fields.Nested(PlainUserSchema(), dump_only=True)
 
 
 class UserSchema(PlainUserSchema):
+    """
+    Schema for representing user data.
+
+    Attributes:
+        received_emails (list): List of received emails.
+        sent_emails (list): List of sent emails.
+    """
     received_emails = fields.List(fields.Nested(PlainEmailSchema()), dump_only=True)
     sent_emails = fields.List(fields.Nested(PlainEmailSchema()), dump_only=True)
 
